d4f8f930744cc3ddd5d83c026c18e9120d53baa5
Babu Izhan <babu.izhan@shopup.org>
2025-03-01 14:41:48 +0530

    create progress bar

--- react-ui-prep/src/projects/progress-bar/index.tsx
+++ react-ui-prep/src/projects/progress-bar/index.tsx
@@ -1,9 +1,38 @@
-const ProgressBar = () => {
+import { useEffect, useState } from "react";
+import "./progressbar.css";
+
+const ProgressBar = ({ progress }: { progress: number }) => {
+  const [animatedProgress, setAnimatedProgress] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => setAnimatedProgress(progress), 100);
+  }, [progress]);
+
   return (
-    <div>
-      <div>Progress Bar</div>
+    <div className="progress-bar-wrapper">
+      <div
+        className="progress-bar"
+        style={{
+          transform: `translateX(${animatedProgress - 100}%)`,
+        }}
+      >
+        {animatedProgress}%
+      </div>
     </div>
   );
 };
 
-export default ProgressBar;
+const ProgressBars = () => {
+  const percentageArray = [5, 10, 20, 30, 40, 50, 60, 70, 100];
+
+  return (
+    <div>
+      <div>Progress Bar</div>
+      {percentageArray.map((p) => {
+        return <ProgressBar progress={p} />;
+      })}
+    </div>
+  );
+};
+
+export default ProgressBars;
